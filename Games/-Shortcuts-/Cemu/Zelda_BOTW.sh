a92e744243a44beeebb16a8a0334fdb2db0415b4
GoVi-48 <alexpeed48@gmail.com>
2020-06-18 03:24:33 +0200

    Updated

--- Games/-Shortcuts-/Cemu/Zelda_BOTW.sh
+++ Games/-Shortcuts-/Cemu/Zelda_BOTW.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
  
-gamemoderun && mangohud WINEPREFIX=~/Wine/wine-pfx_dxvk ~/Wine/wine-build_5.10-tkg/bin/wine64 $HOME"/Games/Emulators/Cemu/Cemu.exe" -g "Z:/Media/Games/Nintendo/Wii U/The Legend of Zelda Breath of the Wild/code/U-King.rpx" -f
+gamemoderun && mangohud WINEPREFIX=~/Wine/wine-pfx_dxvk ~/Wine/wine-build_5.10-tkg/bin/wine64 $HOME"/Games/Emulators/Cemu/Cemu.exe" -g "M:/Games/Nintendo/Wii U/The Legend of Zelda Breath of the Wild/code/U-King.rpx" -f
--- Games/-Shortcuts-/Green_Hell.sh
+++ Games/-Shortcuts-/Green_Hell.sh
@@ -1,3 +1,3 @@
  #!/usr/bin/env bash
  
-gamemoderun && mangohud WINEPREFIX=~/Wine/wine-pfx_dxvk ~/Wine/wine-build_5.10-tkg/bin/wine64 "/Media/Games/PC/Portable/Green Hell/GH.exe"
+gamemoderun && mangohud WINEPREFIX=~/Wine/wine-pfx_dxvk ~/Wine/wine-build_5.10-tkg/bin/wine64 "M:/Games/PC/Portable/Green Hell/GH.exe"

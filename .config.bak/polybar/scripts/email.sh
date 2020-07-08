64e27635dd02c0ca58e402a58cae0e671deceb96
GoVi-48 <alexpeed48@gmail.com>
2020-07-08 17:20:09 +0200

    Updated

--- .config.bak/polybar/scripts/email.sh
+++ .config.bak/polybar/scripts/email.sh
@@ -29,10 +29,21 @@ awk -F '<entry>' '{for (i=2; i<=NF; i++) {print $i}}' |
 sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\2 - \1/p"
 )
 
+notf_sound=$HOME/.local/share/sounds/cause-and-effect.ogg
+notf_off=$HOME/.config/polybar/scripts/not_off
+
 if [ $email_count -eq 0 ]; then
     echo "📭"
+    rm -rf $notf_off
+
+elif [ $email_count -gt 0 ] && [ ! -f "$notf_off" ] && ! pgrep -x "vlc|elisa|kodi" > /dev/null; then
+    echo "📬 $email_count"
+    kdialog --icon "$HOME/.local/share/icons/GoVi-Ic/apps/64/gmail.svg" --title "📬 $email_count" --passivepopup "$email1_title $email2_title" 8 &
+    paplay $notf_sound
+    touch $notf_off
+
 else
     echo "📬 $email_count"
     kdialog --icon "$HOME/.local/share/icons/GoVi-Ic/apps/64/gmail.svg" --title "📬 $email_count" --passivepopup "$email1_title $email2_title" 8 &
-    paplay $HOME/.local/share/sounds/cause-and-effect.ogg
+
 fi

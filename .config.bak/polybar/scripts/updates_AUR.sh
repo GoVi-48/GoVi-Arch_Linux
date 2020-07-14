da2347d01c87ae6d7995a912ef5d26499a1cfb1f
GoVi-48 <alexpeed48@gmail.com>
2020-07-15 01:47:10 +0200

    Updated

--- .config.bak/polybar/scripts/updates_AUR.sh
+++ .config.bak/polybar/scripts/updates_AUR.sh
@@ -12,11 +12,11 @@ if [ $updates_AUR -eq 0 ]; then
     fi
 
 elif [ $updates_AUR -gt 0 ] && [ ! -f "$sound_AUR_off" ]; then
-    echo %{F#e29519}"%{T3}7  %{T-}%{F-}$updates_AUR AUR"
+    echo %{F#e29519}"%{T3}7 %{T-}%{F-}$updates_AUR AUR"
     kdialog --icon "$HOME/.local/share/icons/GoVi-Ic/apps/64/updates-AUR.svg" --passivepopup "$updates_AUR AUR Updates" 8
     paplay $HOME/.local/share/sounds/long-dang.ogg
     touch $sound_AUR_off
     
 else
-    echo %{F#e29519}"%{T3}7  %{T-}%{F-}$updates_AUR AUR"
+    echo %{F#e29519}"%{T3}7 %{T-}%{F-}$updates_AUR AUR"
 fi

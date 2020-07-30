392e0bf92d2a9c353fcd60d9e2c60b61cbdb8bbb
GoVi-48 <alexpeed48@gmail.com>
2020-07-30 16:01:30 +0200

    Updated

--- Scripts/Bash/Games/NMS.sh
+++ Scripts/Bash/Games/NMS.sh
@@ -4,7 +4,7 @@ killall polybar
 
 qdbus org.kde.KWin /Compositor suspend
 
-gamemoderun WINEPREFIX=~/Wine/wine-pfx_5.13-dxvk-1.7-mf ~/Wine/wine-build_5.13-tkg/bin/wine64 $HOME"/Games/-Library-/PC/No Man's Sky/Binaries/NMS.exe"
+gamemoderun mangohud WINEPREFIX=~/Wine/wine-pfx_5.13-dxvk-1.7-mf ~/Wine/wine-build_5.13-tkg/bin/wine64 $HOME"/Games/-Library-/PC/No Man's Sky/Binaries/NMS.exe"
 
 while pgrep -x "wineserver" > /dev/null; do sleep 1; done
 

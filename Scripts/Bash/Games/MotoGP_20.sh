b68687c78be6a29adf0b9876eb12f0d656159c60
GoVi-48 <alexpeed48@gmail.com>
2020-09-09 22:23:01 +0200

    Updated

--- Scripts/Bash/Games/MotoGP_20.sh
+++ Scripts/Bash/Games/MotoGP_20.sh
@@ -4,7 +4,7 @@ killall polybar
 qdbus org.kde.KWin /Compositor suspend
 
 export WINEPREFIX=~/Wine/wine-pfx_5.16/wine-pfx_5.16-dxvk-1.7.1-mf
-export WINE=~/Wine/wine-build_5.16-tkg/usr/bin/wine64
+export WINE=~/Wine/wine-build_Lutris/wine-build_5.7.8-lutris/bin/wine64
 
 cd "$HOME/Games/-Library-/PC/MotoGP 20"
 gamemoderun $WINE "motogp20.exe"

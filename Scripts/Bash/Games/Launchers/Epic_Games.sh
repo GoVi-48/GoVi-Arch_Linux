6a7cd4f130b771b7c58306be2142492787bec643
GoVi-48 <alexpeed48@gmail.com>
2020-09-26 05:11:16 +0200

    Updated

--- Scripts/Bash/Games/Launchers/Epic_Games.sh
+++ Scripts/Bash/Games/Launchers/Epic_Games.sh
@@ -1,7 +1,12 @@
  #!/usr/bin/env bash
 
-export WINEPREFIX=~/Wine/wine-pfx_5.17/wine-pfx_mf
-export WINE=~/Wine/wine-build_5.17-tkg/usr/bin/wine64
- 
-cd "$HOME/Games/Epic Games/Launcher/Engine/Binaries/Win64"
-gamemoderun $WINE "EpicGamesLauncher.exe"
+export WINEPREFIX=~/Wine/wine-pfx_5.17/wine-pfx_dxvk-1.7.1-mf
+export WINE=$HOME/Wine/wine-build_Lutris/wine-build_5.7.10-lutris/bin/wine64
+
+_GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+
+cd "$HOME/Wine/wine-pfx_5.17/wine-pfx_dxvk-1.7.1-mf/drive_c/Program Files (x86)/Epic Games/Launcher/Engine/Binaries/Win64"
+gamemoderun $WINE "EpicGamesLauncher.exe" -opengl -SkipBuildPatchPrereq
+
+while pgrep -x "EpicGamesLaunch" > /dev/null; do sleep 1; done
+    killall gamemoded
--- Scripts/Bash/Games/Rocket_League.sh
+++ Scripts/Bash/Games/Rocket_League.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
-qdbus org.kde.KWin /Compositor suspend
-killall latte-dock
-killall polybar
-killall plasmashell
+# export WINEPREFIX=$HOME/Wine/wine-pfx_lutris/wine-pfx_dxvk-1.7.1-mf
+# export WINE=$HOME/Wine/wine-build_Lutris/wine-build_5.7.10-lutris/bin/wine64
+# 
+# cd "/Datos/Games/Epic Games/rocketleague/Binaries/Win64"
+# gamemoderun $WINE "RocketLeague.exe" -epicportal
 
-export WINEDLLOVERRIDES="mscoree,mshtml="
-export WINEPREFIX=$HOME/Wine/wine-pfx_lutris/wine-pfx_Rocket_League
+export WINEPREFIX=~/Wine/wine-pfx_5.17/wine-pfx_dxvk-1.7.1-mf
 export WINE=$HOME/Wine/wine-build_Lutris/wine-build_5.7.10-lutris/bin/wine64
 
-cd "/Datos/Games/Epic Games/rocketleague/Binaries/Win64"
-gamemoderun $WINE "RocketLeague.exe" -epicportal
+_GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+
+cd "$HOME/Wine/wine-pfx_5.17/wine-pfx_dxvk-1.7.1-mf/drive_c/Program Files (x86)/Epic Games/Launcher/Engine/Binaries/Win64"
+gamemoderun $WINE "EpicGamesLauncher.exe" -opengl -SkipBuildPatchPrereq
+
+if pgrep -x "RocketLeague.exe" > /dev/null
+    qdbus org.kde.KWin /Compositor suspend
+    killall latte-dock
+    killall polybar
+    killall plasmashell
+fi
 
 sleep 5
 
-while pgrep -x "RocketLeague.exe" > /dev/null; do sleep 1; done
+if ! pgrep -x "EpicGamesLaunch" > /dev/null; do sleep 1; done
     killall lutris
     killall gamemoded
     qdbus org.kde.KWin /Compositor resume
     $HOME/Scripts/Bash/Polybar
     latte-dock &
     plasmashell > /dev/null 2>&1 & exit
+fi
+    
+# com.epicgames.launcher://apps/Sugar?action=launch&silent=true

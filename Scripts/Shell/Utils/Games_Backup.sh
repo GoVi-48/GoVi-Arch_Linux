a2aa40d0f35e04658efd49e6003a19e57141bbe1
GoVi-48 <alexpeed48@gmail.com>
2021-03-22 21:25:58 +0100

    Updated

---
+++ Scripts/Shell/Utils/Games_Backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Valheim
+rsync -arv --delete "/Media/Games/PC/Valheim/Save Game/" "/Media/[SaveData]/Games/Save Games/Valheim/"
+
+# Satisfactory
+rsync -arv --delete "/Media/Games/PC/Satisfactory/Save Game/" "/Media/[SaveData]/Games/Save Games/Satisfactory/FactoryGame/"

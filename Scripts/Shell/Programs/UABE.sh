8ce191a08a828f801b20879607424f52b707e756
GoVi-48 <alexpeed48@gmail.com>
2021-03-29 14:16:40 +0200

    Updated

---
+++ Scripts/Shell/Programs/UABE.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+WINEPREFIX="$HOME/Programs/Assets-Extractor/Wine/wine-pfx" "$HOME/Programs/Assets-Extractor/Wine/wine"  "$HOME/Programs/Assets-Extractor/AssetsBundleExtractor_2.2stabled_64bit/AssetBundleExtractor.exe"
+
---
+++ Scripts/Shell/Programs/Unity_Ripper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+WINEPREFIX="$HOME/Programs/Assets-Extractor/Wine/wine-pfx" "$HOME/Programs/Assets-Extractor/Wine/wine" "$HOME/Programs/Assets-Extractor/uTinyRipper/uTinyRipper.exe"
+
